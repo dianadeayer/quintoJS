2c561fe0531bbea4b70c1df0d59261afaad52b58
Diana Aguiñaga <dianacarolina.ag14@gmail.com>
2025-06-06 08:36:58 -0600

    creatinng styles for the songs sections

---
+++ script.js
@@ -0,0 +1,31 @@
+import data from "./albums.js";
+
+const artist = data.artistUnion.profile.name;
+const albums = data.artistUnion.discography.albums.items;
+
+albums.forEach((albumItem) => {
+  const release = albumItem.releases.items[0];
+  const albumDiv = document.createElement("div");
+  albumDiv.innerHTML = `<h2>${release.name} (${release.date.year})</h2>`;
+
+  const table = document.createElement("table");
+  table.innerHTML = `
+  <thead>
+    <tr><th>#</th><th>Título</th><th>Duración</th></tr>
+  </thead>
+  <tbody></tbody>
+`;
+  const tbody = table.querySelector("tbody");
+  release.tracks.items.forEach((trackItem, index) => {
+    const track = trackItem.track;
+    const row = document.createElement("tr");
+    const durationSec = Math.round(track.duration.totalMilliseconds / 1000);
+    row.innerHTML = `<td>${index + 1}</td><td>${
+      track.name
+    }</td><td>${durationSec}</td>`;
+    tbody.appendChild(row);
+  });
+
+  albumDiv.appendChild(table);
+  document.body.appendChild(albumDiv);
+});
