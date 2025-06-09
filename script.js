0cee9488bcff2f6d9979f336d2ded77662028c94
Diana Aguiñaga <dianacarolina.ag14@gmail.com>
2025-06-09 14:47:46 -0600

    added animations, play button for every album and fixed information in header

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ const artist = data.artistUnion.profile.name;
 const albums = data.artistUnion.discography.albums.items;
 const headerImage = data.artistUnion.visuals.headerImage.sources[0].url;
 const monthlyListeners = data.artistUnion.stats.monthlyListeners;
+const verified = data.artistUnion.profile.verified;
 
-const coverHeader = document.createElement("div");
-coverHeader.className = "artist-cover";
-coverHeader.innerHTML = `
+const createArtistHeader = (headerImage, artist, monthlyListeners) => {
+  const coverHeader = document.createElement("div");
+  coverHeader.className = "artist-cover";
+  coverHeader.innerHTML = `
   <img src="${headerImage}" alt="Artist cover" class="artist-cover-img">
   <div class="artist-cover-info">
+  <p class="artist-verified">${verified ? "Verified Artist" : ""}</p>
     <h1 class="artist-name">${artist}</h1>
     <p class="artist-listeners">${monthlyListeners.toLocaleString()} monthly listeners</p>
   </div>
 `;
-document.body.prepend(coverHeader);
+  return coverHeader;
+};
+
+document.body.prepend(
+  createArtistHeader(headerImage, artist, monthlyListeners)
+);
 
 const createTracksTable = (tracks) => {
   const table = document.createElement("table");
@@ -42,24 +50,29 @@ const createTracksTable = (tracks) => {
   return table;
 };
 
-albums.forEach((albumItem) => {
-  const release = albumItem.releases.items[0];
+const createAlbumBlock = (release) => {
   const albumDiv = document.createElement("div");
-
   const coverUrl = release.coverArt.sources[0].url;
-
   albumDiv.innerHTML = `
     <div class="album-header">
-      <img src="${coverUrl}" alt="cover" class="album-cover">
-      <div class="album-meta">
-      <h2 class="album-title">${release.name} </h2>
-      <p class= "album-info"> Album · ${release.date.year} · ${release.tracks.items.length} songs</p>
+    <img src="${coverUrl}" alt="cover" class="album-cover">
+    <div class="album-meta">
+      <h2 class="album-title">${release.name}</h2>
+      <p class="album-info">Album · ${release.date.year} · ${release.tracks.items.length} songs</p>
+      <button class="play-button" aria-label="Reproducir álbum">
+        <svg viewBox="0 0 16 16">
+          <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
+        </svg>
+      </button>
     </div>
-      </div>
-  `;
-
+  </div>
+`;
   const table = createTracksTable(release.tracks.items);
-
   albumDiv.appendChild(table);
-  document.body.appendChild(albumDiv);
+  return albumDiv;
+};
+
+albums.forEach((albumItem) => {
+  const release = albumItem.releases.items[0];
+  document.body.appendChild(createAlbumBlock(release));
 });
